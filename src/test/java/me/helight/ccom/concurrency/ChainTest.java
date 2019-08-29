0886cd71d780ed60ddb7b07991bc16e098f1f102
Christoph Feuerer <helightdev@gmail.com>
2019-08-29 13:05:35 +0200

    Removed Unwanted Debug

--- src/test/java/me/helight/ccom/concurrency/ChainTest.java
+++ src/test/java/me/helight/ccom/concurrency/ChainTest.java
@@ -1,8 +1,11 @@
 package me.helight.ccom.concurrency;
 
 import me.helight.ccom.concurrency.chain.objectives.ConsumerObjective;
+import me.helight.ccom.concurrency.chain.objectives.RunnableObjective;
 import me.helight.ccom.concurrency.chain.objectives.SupplierObjective;
 import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,15 +13,20 @@ class ChainTest {
 
     @Test
     void test() {
+        AtomicBoolean runnableCalled = new AtomicBoolean(false);
         long duration = Chain.create()
                 .sleep(100)
                 .addObjective(new SupplierObjective(() -> 10))
                 .sleep(100)
+                .addObjective(new RunnableObjective(() -> {
+                    runnableCalled.set(true);
+                }))
                 .addObjective(new ConsumerObjective(first -> {
                     assertEquals(10, first);
                 }, 1))
                 .run();
 
+        assertTrue(runnableCalled.get());
         assertTrue(duration >= 200);
     }
 
